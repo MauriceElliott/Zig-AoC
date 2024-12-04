12eeacd5c31b4f6de65ea521e0ba3155f6c8212f
MauriceElliott <maurice.elliott@telusagcg.com>
2024-12-04 23:55:01 +0000

    Finished Day03

--- src/day03.zig
+++ src/day03.zig
@@ -6,11 +6,22 @@ const State: type = enum {
     u,
     l,
     par_open,
-    par_close,
-    n1,
-    n2,
-    comma,
+    n1_start,
+    n1_continued,
+    n1_end,
+    n2_start,
+    n2_continued,
+    n2_end,
     none,
+    d,
+    o,
+    n,
+    s_quote,
+    t,
+    open_par_do,
+    close_par_do,
+    open_par_dont,
+    close_par_dont,
 };
 
 fn parseCharacter(character: u8, current_state: State) State {
@@ -30,22 +41,135 @@ fn parseCharacter(character: u8, current_state: State) State {
                 return State.none;
             }
         },
+        '(' => {
+            if (current_state == State.l) {
+                return State.par_open;
+            }
+            if (current_state == State.o) {
+                return State.open_par_do;
+            } else if (current_state == State.t) {
+                return State.open_par_dont;
+            } else {
+                return State.none;
+            }
+        },
+        ')' => {
+            if (current_state == State.n2_continued or current_state == State.n2_start) {
+                return State.n2_end;
+            } else if (current_state == State.open_par_do) {
+                return State.close_par_do;
+            } else if (current_state == State.open_par_dont) {
+                return State.close_par_dont;
+            } else {
+                return State.none;
+            }
+        },
+        48...57 => {
+            if (current_state == State.par_open) {
+                return State.n1_start;
+            } else if (current_state == State.n1_start) {
+                return State.n1_continued;
+            } else if (current_state == State.n1_continued) {
+                return State.n1_continued;
+            } else if (current_state == State.n1_end) {
+                return State.n2_start;
+            } else if (current_state == State.n2_start) {
+                return State.n2_continued;
+            } else if (current_state == State.n2_continued) {
+                return State.n2_continued;
+            } else {
+                return State.none;
+            }
+        },
+        ',' => {
+            if (current_state == State.n1_continued or current_state == State.n1_start) {
+                return State.n1_end;
+            } else {
+                return State.none;
+            }
+        },
+        'd' => State.d,
+        'o' => {
+            if (current_state == State.d) {
+                return State.o;
+            } else {
+                return State.none;
+            }
+        },
+        'n' => {
+            if (current_state == State.o) {
+                return State.n;
+            } else {
+                return State.none;
+            }
+        },
+        '\'' => {
+            if (current_state == State.n) {
+                return State.s_quote;
+            } else {
+                return State.none;
+            }
+        },
+        't' => {
+            if (current_state == State.s_quote) {
+                return State.t;
+            } else {
+                return State.none;
+            }
+        },
         else => State.none,
     };
     return state;
 }
 
+fn parseAndMultiply(action: []const u8) !i64 {
+    const trimmed_action = trim(u8, action, "mul");
+    const index_of_comma: usize = indexOfAny(u8, trimmed_action, ",").?;
+    const index_of_close_par: usize = indexOfAny(u8, trimmed_action, ")").?;
+    const number_1_str: []const u8 = trim(u8, trimmed_action[1..(index_of_comma)], " ");
+    const number_1_int: i64 = try parseInt(i64, number_1_str, 10);
+    const number_2_str: []const u8 = trim(u8, trimmed_action[(index_of_comma + 1)..(index_of_close_par)], " ");
+    const number_2_int: i64 = try parseInt(i64, number_2_str, 10);
+    const total: i64 = (number_1_int * number_2_int);
+    return total;
+}
+
 pub fn main() !void {
-    const memory = "xxmuxul"; //"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const memory = data;
     var character_state: State = State.none;
+    var total: i64 = 0;
+    var enabled: bool = true;
+    var start_index: ?usize = undefined;
+    var end_index: ?usize = undefined;
     for (0..memory.len) |s| {
         character_state = parseCharacter(memory[s], character_state);
+        if (character_state == State.close_par_do) {
+            enabled = true;
+        }
+        if (character_state == State.close_par_dont) {
+            enabled = false;
+        }
+        if (character_state == State.m) {
+            start_index = s;
+        } else if (character_state == State.none) {
+            start_index = null;
+        }
+        if (character_state == State.n2_end) {
+            end_index = s;
+        } else if (character_state == State.none) {
+            end_index = null;
+        }
+        if (start_index != null and end_index != null) {
+            const i_start: usize = start_index.?;
+            const i_end: usize = (end_index.? + 1);
+            start_index = null;
+            end_index = null;
+            if (enabled == true) {
+                total += try parseAndMultiply(memory[i_start..i_end]);
+            }
+        }
     }
-    if (character_state == State.l) {
-        print("correct! \n", .{});
-    } else {
-        print("Incorrect! \n", .{});
-    }
+    print("correct! {}\n", .{total});
 }
 
 // Useful stdlib functions
