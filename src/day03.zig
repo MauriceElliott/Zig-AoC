72d6055b22d2e1efba1fc0dbe10760766e9e09b7
MauriceElliott <maurice.elliott@telusagcg.com>
2024-12-04 23:56:57 +0000

    Cleanup

--- src/day03.zig
+++ src/day03.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 const data = @embedFile("data/day03.txt");
+const indexOfAny = std.mem.indexOfAny;
+const trim = std.mem.trim;
+const parseInt = std.fmt.parseInt;
+const print = std.debug.print;
 
 const State: type = enum {
     m,
@@ -169,36 +173,5 @@ pub fn main() !void {
             }
         }
     }
-    print("correct! {}\n", .{total});
+    print("total: {}\n", .{total});
 }
-
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
-
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
-
-const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
-
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
